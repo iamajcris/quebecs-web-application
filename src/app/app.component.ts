47e53a99abe078607382aac493f8c3bda491e174
iamajcris <aldrinjohn.quebec@gmail.com>
2023-06-20 21:32:41 +0800

    Setup bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB0Kbahoo5eam5AjMznev_v0d_QYT8S9bg",
-  authDomain: "quebecs-system.firebaseapp.com",
-  projectId: "quebecs-system",
-  storageBucket: "quebecs-system.appspot.com",
-  messagingSenderId: "971975771640",
-  appId: "1:971975771640:web:55ff644b4033d7e7b6820d",
-  measurementId: "G-G139SSH94D"
-};
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -26,9 +12,16 @@ const firebaseConfig = {
 export class AppComponent implements OnInit {
   title = 'quebecs-web-application';
 
+  constructor(private modalService: NgbModal) {
+  }
+
   ngOnInit() {
     // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+    const app = initializeApp(environment.firebaseConfig);
     const analytics = getAnalytics(app);
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
   }
 }
